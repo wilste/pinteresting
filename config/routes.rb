f11a092e47d050f57f1e3e0ddc74f159ab646f7e
William Stegner <william@factortechnologies.com>
2015-05-31 20:58:33 -0700

    Added pages and navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  #get 'pages/home'  <-- Old route created during generate
+  root "pages#home"  #This is the root_path
+  get "about" => "pages#about"  #Creates about_path
+  get "test" => "pages#test"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
